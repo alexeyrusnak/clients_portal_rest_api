e5029de334937742b3c20cb63e73a35f6043f26f
Alexey Rusnak <alexeyrusnak@gmail.com>
2018-05-25 11:59:41 +0300

    4000

--- templates_messages.sql
+++ templates_messages.sql
@@ -1,40 +1,13 @@
-prompt Importing table templates_messages...
-set feedback off
-set define off
-insert into templates_messages (TM_ID, DEF, DEF_XML, APPL_APPL_ID, MESSAGES_TEXT, MESSAGES_TEXT_XML, STORE_DAYS, DEL_DATE, DEL_USER, IS_ORDER)
-values (tm_seq.nextval, 'Заявка <номер заявки> принята в работу', '<CAP><RU>Заявка <номер заявки> принята в работу</RU><EN>Zajavka <nomer zajavki> prinjata v rabotu</EN></CAP>', 21, 'Добрый день! 
-
-Заявка <номер заявки> на обработку Вашего груза "<груз>" принята в работу <дата>. 
-
-С уважением, 
-ООО "МКС-форвардинг', '<CAP><RU>Добрый день! 
-
-Заявка <номер заявки> на обработку Вашего груза "<груз>" принята в работу <дата>. 
-
-С уважением, 
-ООО "МКС-форвардинг</RU><EN>Dobryjj den`! 
-
-Zajavka <nomer zajavki> na obrabotku Vashego gruza "<gruz>" prinjata v rabotu <data>. 
-
-S uvazheniem, 
-OOO "MKS-forvarding</EN></CAP>', 1, null, null, 0);
-
-insert into templates_messages (TM_ID, DEF, DEF_XML, APPL_APPL_ID, MESSAGES_TEXT, MESSAGES_TEXT_XML, STORE_DAYS, DEL_DATE, DEL_USER, IS_ORDER)
-values (tm_seq.nextval, 'Изменение статуса заказа <номер заявки>', '<CAP><RU>Изменение статуса заказа <номер заявки></RU><EN>Izmenenie statusa zakaza <nomer zajavki></EN></CAP>', 21, 'Изменение статуса заказа <номер заявки>
-
-Уважаемый клиент, 
-
-<дата> статус Вашего заказа изменен c "<старый статус>" на "<новый статус>"', '<CAP><RU>Изменение статуса заказа <номер заявки>
-
-Уважаемый клиент, 
-
-<дата> статус Вашего заказа изменен c "<старый статус>" на "<новый статус>"</RU><EN>Izmenenie statusa zakaza <nomer zajavki>
-
-Uvazhaemyjj klient, 
-
-<data> status Vashego zakaza izmenen c "<staryjj status>" na "<novyjj status>"</EN></CAP>', 1, null, null, null);
-
-commit;
-
-prompt Done.
-
+create or replace view templates_messages as
+select
+       tm_id,
+       cast(substr(multilang.get_language_cap(def),1,500) as varchar(500)) def,
+       def def_xml,
+       appl_appl_id,
+       cast(substr(multilang.get_language_cap(messages_text),1,4000) as varchar(4000)) messages_text,
+       messages_text messages_text_xml,
+       store_days,
+       del_date,
+       del_user,
+     is_order
+from templates_messages_dic;
